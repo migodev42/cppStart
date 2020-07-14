dd6843f677afad0088bff925808bbd33c821d6fc
LeoooY <leoy.dev@gmail.com>
2020-07-14 13:31:48 +0800

    compare str

---
+++ practice/compareStr.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+int main(){
+    using std::string;
+    using std::cout;
+    string a="hello";
+    string b="hallo";
+    if(a>b){
+        cout << ">\n";
+    }else{
+        cout << "<\n";
+    }
+    
+    return 0;
+}
