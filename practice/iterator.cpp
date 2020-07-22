9f3ed9187f69e9771186a9e2349a2d3c1a45acb8
LeoooY <leoy.dev@gmail.com>
2020-07-22 18:07:19 +0800

    箭头运算符取成员

--- practice/iterator.cpp
+++ practice/iterator.cpp
@@ -1,15 +1,18 @@
 #include <cctype>
 #include <iostream>
 #include <string>
-
+#include <vector>
 int main() {
     using std::cin;
     using std::cout;
     using std::endl;
+
     using std::string;
-    using std::toupper;
+    using std::vector;
+
     using std::isspace;
-    
+    using std::toupper;
+
     string s = "some string";
     cout << "origin: " << s << endl;
 
@@ -20,10 +23,23 @@ int main() {
     cout << "result: " << s << endl;
 
     /* 当遇到空格或者到迭代器尾部时候循环停止 */
-    for (auto el = s.begin(); el !=s.end() && !isspace(*el) ; ++el) {
+    for (auto el = s.begin(); el != s.end() && !isspace(*el); ++el) {
         *el = toupper(*el);
-    }    
+    }
     cout << "result: " << s << endl;
-    
+
+    vector<string> text = {
+        "Contrary to popular belief, Lorem Ipsum is not simply random text. It "
+        "has roots in a piece of classical Latin literature from 45 BC, making "
+        "it over 2000 years old. ",
+        "The standard chunk of Lorem Ipsum used since the 1500s is reproduced "
+        "below for those interested. "};
+
+    /* 输出text中的每段文字 */
+    cout << "输出text的第一段文字:" << endl;
+    for (auto el = text.cbegin(); el != text.cend() && !el->empty(); ++el) {
+        cout << *el << endl;
+    }
+
     return 0;
 }
