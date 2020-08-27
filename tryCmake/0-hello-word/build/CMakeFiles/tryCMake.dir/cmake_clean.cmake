c168a6207acd9c080183b3a113ac7e2431e4d8d8
LeoooY <leoy.dev@gmail.com>
2020-08-27 12:25:13 +0800

    try cmake

---
+++ tryCmake/0-hello-word/build/CMakeFiles/tryCMake.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/tryCMake.dir/tutorial.cxx.o"
+  "tryCMake"
+  "tryCMake.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/tryCMake.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ tryCmake/2-static-library/build/CMakeFiles/library.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/library.dir/compute.cpp.o"
+  "liblibrary.a"
+  "liblibrary.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/library.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
