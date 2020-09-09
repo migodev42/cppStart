a49d626544dbc54c251f6e3cda843e490ca3b563
LeoooY <leoy.dev@gmail.com>
2020-09-09 18:40:29 +0800

    ostream_iterator

--- tryQt/helloworld/index.cpp
+++ tryQt/helloworld/index.cpp
@@ -1,13 +1,22 @@
 #include <iostream>
 #include <vector>
 #include <string>
+#include <algorithm>
+#include <ostream>
+#include <iterator> 
 
 using std::vector;
 using std::string;
 using std::cout;
 using std::cin;
 using std::endl;
+using std::copy;
+using std::ostream_iterator;
 
 int main(){
+    vector<int> vec = {1 ,2 ,3, 4, 5};
+    ostream_iterator<int> out_iter(cout, " ");
+    copy(vec.begin(), vec.end(), out_iter);
+    cout << endl;
     return 0;
 }
