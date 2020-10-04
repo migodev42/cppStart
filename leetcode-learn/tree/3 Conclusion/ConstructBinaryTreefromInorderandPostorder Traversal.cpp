0b72c0beddc58c1545cc0bc2c15b2cf5e877bc02
LeoooY <leoy.dev@gmail.com>
2020-10-04 10:03:13 +0800

    TODO: Construct Binary Tree from Inorder and Postorder Traversal

---
+++ leetcode-learn/tree/3 Conclusion/ConstructBinaryTreefromInorderandPostorder Traversal.cpp
@@ -0,0 +1,35 @@
+#include <vector>
+#include <algorithm>
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+using std::vector;
+using std::max;
+
+struct TreeNode {
+    int val;
+    TreeNode* left;
+    TreeNode* right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode* left, TreeNode* right) : val(x), left(left), right(right) {}
+};
+class Solution {
+public:
+    void traverse(TreeNode *root, vector<int> &leftsub, vector<int> &rightsub, int postorder){
+            
+    }
+    TreeNode* buildTree(vector<int>& inorder, vector<int>& postorder) {
+        
+        TreeNode *root = new ThreeNode();
+        traverse()
+    }
+};
