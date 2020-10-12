4dcd4f399ec858cfed20a45b76dd2f98c6c85f65
LeoooY <leoy.dev@gmail.com>
2020-10-12 19:30:36 +0800

    n皇后

---
+++ leetcode/51.hard.N皇后.cpp
@@ -0,0 +1,22 @@
+#include <vector>
+#include <string>
+
+using std::vector;
+using std::string;
+
+class Solution {
+public:
+    void backtrack(int n, vector<vector<string>>& rs) {
+        // if () {
+            // rs.push_back();
+        // }
+        for(auto i=0; i<n; ++i){
+            
+        }
+    }
+    vector<vector<string>> solveNQueens(int n) {
+        vector<vector<string>> rs;
+        backtrack(n, rs);
+        return rs;
+    }
+};
