5b170edd0326ee31050ab53c46663d372f3f220a
LeoooY <leoy.dev@gmail.com>
2020-10-13 12:20:14 +0800

    n皇后

--- leetcode/51.hard.N皇后.cpp
+++ leetcode/51.hard.N皇后.cpp
@@ -1,22 +1,47 @@
-#include <vector>
-#include <string>
-
-using std::vector;
-using std::string;
-
 class Solution {
 public:
-    void backtrack(int n, vector<vector<string>>& rs) {
-        // if () {
-            // rs.push_back();
-        // }
-        for(auto i=0; i<n; ++i){
-            
+    bool isValid(vector<string> board, int row, int col) {        
+        // 检查当前列有没有重复的
+        for(auto i = 0; i<row; ++i){
+            if(board[col][i] == 'Q') return false;
+        }
+
+        // 检查左上方
+        for(auto irow = row-1, icol = col-1;
+            irow >=0 && icol>=0;
+            --irow, --icol){
+            if(board[icol][irow] == 'Q') return false;
+        }
+
+        // 检查右上方
+        for(auto irow = row-1, icol = col+1;
+            irow >=0 && icol< board.size(); 
+            --irow, ++icol){
+            if(board[icol][irow] == 'Q') return false;
+        }
+        return true;
+
+    }
+    void backtrack(vector<string> board, vector<vector<string>>& rs, int currow) {
+        int n = board[0].size();
+        if (currow == n) {
+            rs.push_back(board);
+            return;
+        }        
+        for (auto col = 0; col < n; ++col) {
+            if (!isValid(board, currow, col)) {
+                continue;
+            }
+            board[col][currow] = 'Q';
+            backtrack(board, rs, currow + 1);
+            board[col][currow] = '.';
         }
     }
     vector<vector<string>> solveNQueens(int n) {
         vector<vector<string>> rs;
-        backtrack(n, rs);
+        // 初始化 n*n 的棋盘
+        vector<string> board(n, string(n, '.'));
+        backtrack(board, rs, 0);
         return rs;
     }
 };
