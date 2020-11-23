d300cb4c692a2aae6f0252bc39bc0b2b055e405e
LeoooY <leoy.dev@gmail.com>
2020-11-23 19:47:53 +0800

    TODO mergesortedArray

--- leetcode-learn/arrays101/2InsertItemsIntoAnArray/MergeSortedArray.cpp
+++ leetcode-learn/arrays101/2InsertItemsIntoAnArray/MergeSortedArray.cpp
@@ -1,17 +1,36 @@
 #include <vector>
 
 using std::vector;
+/*
+    Input:
+    nums1 = [1,2,3,0,0,0], m = 3
+    nums2 = [2,5,6],       n = 3
+
+    Output: [1,2,2,3,5,6]
+*/
 
 class Solution {
 public:
-    void resizeAndMove(vector<int>& target, int frompos) {
-        target.resize(target.size() + 1);
+    void move(vector<int>& target, int frompos) {
         for (int i = target.size() - 1; i > frompos; --i) {
             target[i] = target[i - 1];
         }
 
     }
     void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
-
+        int pos1 = 0;
+        int pos2 = 0;
+        while (pos1 < m && pos2 < n) {
+            if (nums1[pos1] < nums2[pos2]) {
+                ++pos1;
+            }
+            else {
+                move(nums1, pos1);
+                nums1[pos1] = nums2[pos2++];
+            }
+        }
+        while (pos2 < n) {
+            nums1[pos1] = nums2[pos2++];            
+        }
     }
 };
