f79d5656f97c2f96c941652c91b1f2106668d30c
LeoooY <leoy.dev@gmail.com>
2020-11-30 10:35:32 +0800

    leetcode 976

---
+++ leetcode/976.easy.三角形的最大周长.cpp
@@ -0,0 +1,15 @@
+#include <vector>
+#include <algorithm>
+#include <deque>
+
+using std::max;
+using std::vector;
+using std::deque;
+
+/* 贪心算法+排序 */
+class Solution {
+public:    
+    int largestPerimeter(vector<int>& A) {
+        
+    }
+};
