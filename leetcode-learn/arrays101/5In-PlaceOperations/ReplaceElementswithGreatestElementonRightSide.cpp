482f9a5147e56775354ec244f03dc87e26626e03
LeoooY <leoy.dev@gmail.com>
2020-12-12 23:13:01 +0800

    ReplaceElementswithGreatestElementonRightSide

--- leetcode-learn/arrays101/5In-PlaceOperations/ReplaceElementswithGreatestElementonRightSide.cpp
+++ leetcode-learn/arrays101/5In-PlaceOperations/ReplaceElementswithGreatestElementonRightSide.cpp
@@ -2,21 +2,35 @@
 
 using std::vector;
 
+// class Solution {
+// public:
+//     vector<int> replaceElements(vector<int>& arr) {
+//         for(int i = 0; i< arr.size(); ++i){
+//             for(int j = i; j<arr.size(); ++j){
+//                 if(arr[i]<arr[j]) arr[i]=arr[j];
+//             }
+//         }
+        
+//         int i =1;
+//         while(i<arr.size()){
+//             arr[i-1]= arr[i];
+//             ++i;
+//         }
+//         arr[arr.size()-1]=-1;
+//         return arr;
+//     }
+// };
+
+
 class Solution {
 public:
     vector<int> replaceElements(vector<int>& arr) {
-        for(int i = 0; i< arr.size(); ++i){
-            for(int j = i; j<arr.size(); ++j){
-                if(arr[i]<arr[j]) arr[i]=arr[j];
-            }
+        int maxval = -1;
+        for(int i = arr.size()-1; i>=0; --i){
+            int tmp = arr[i];
+            arr[i]=maxval;
+            maxval = max(tmp,maxval);
         }
-        
-        int i =1;
-        while(i<arr.size()){
-            arr[i-1]= arr[i];
-            ++i;
-        }
-        arr[arr.size()-1]=-1;
         return arr;
     }
 };
