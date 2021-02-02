f22bb25c9a656159a057d760354340f306ee845f
LeoooY <leoy.dev@gmail.com>
2021-02-02 12:14:48 +0800

    daily

---
+++ leetcode/leetcode-extension/424.替换后的最长重复字符.cpp
@@ -0,0 +1,19 @@
+/*
+ * @lc app=leetcode.cn id=424 lang=cpp
+ *
+ * [424] 替换后的最长重复字符
+ */
+
+// @lc code=start
+#include <string>
+
+using std::string;
+
+class Solution {
+public:
+    int characterReplacement(string s, int k) {
+
+    }
+};
+// @lc code=end
+
