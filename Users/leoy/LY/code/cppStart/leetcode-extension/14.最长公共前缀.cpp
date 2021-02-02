25c74f04a158e1c65b50dceb58cee650c7e0dd39
LeoooY <leoy.dev@gmail.com>
2021-02-02 11:20:29 +0800

    leetcode

---
+++ Users/leoy/LY/code/cppStart/leetcode-extension/14.最长公共前缀.cpp
@@ -0,0 +1,56 @@
+/*
+ * @lc app=leetcode.cn id=14 lang=cpp
+ *
+ * [14] 最长公共前缀
+ *
+ * https://leetcode-cn.com/problems/longest-common-prefix/description/
+ *
+ * algorithms
+ * Easy (39.15%)
+ * Likes:    1439
+ * Dislikes: 0
+ * Total Accepted:    440.3K
+ * Total Submissions: 1.1M
+ * Testcase Example:  '["flower","flow","flight"]'
+ *
+ * 编写一个函数来查找字符串数组中的最长公共前缀。
+ * 
+ * 如果不存在公共前缀，返回空字符串 ""。
+ * 
+ * 
+ * 
+ * 示例 1：
+ * 
+ * 
+ * 输入：strs = ["flower","flow","flight"]
+ * 输出："fl"
+ * 
+ * 
+ * 示例 2：
+ * 
+ * 
+ * 输入：strs = ["dog","racecar","car"]
+ * 输出：""
+ * 解释：输入不存在公共前缀。
+ * 
+ * 
+ * 
+ * 提示：
+ * 
+ * 
+ * 0 
+ * 0 
+ * strs[i] 仅由小写英文字母组成
+ * 
+ * 
+ */
+
+// @lc code=start
+class Solution {
+public:
+    string longestCommonPrefix(vector<string>& strs) {
+
+    }
+};
+// @lc code=end
+
---
+++ Users/leoy/LY/code/cppStart/leetcode-extension/15.三数之和.cpp
@@ -0,0 +1,23 @@
+/*
+ * @lc app=leetcode.cn id=15 lang=cpp
+ *
+ * [15] 三数之和
+ */
+#include <vector>
+#include <algorithm>
+
+using std::sort;
+using std::vector;
+
+// @lc code=start
+// n-sum问题 https://www.cnblogs.com/yingying7/p/11044215.html
+class Solution {
+public:
+    vector<vector<int>> threeSum(vector<int>& nums) {
+      int n = nums.size();
+      sort(nums.begin(),nums.end());
+    
+    }
+};
+// @lc code=end
+
