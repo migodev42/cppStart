c1eeb8dabc0551fa7e2b74125d4cf4c0218a25b5
LeoooY <leoy.dev@gmail.com>
2021-02-07 10:52:36 +0800

    leetcode

---
+++ leetcode/leetcode-extension/1208.尽可能使字符串相等.cpp
@@ -0,0 +1,27 @@
+/*
+ * @lc app=leetcode.cn id=1208 lang=cpp
+ *
+ * [1208] 尽可能使字符串相等
+ */
+
+ // @lc code=start
+#include <string>
+#include <vector>
+
+using std::string;
+using std::vector;
+
+
+class Solution {
+public:
+  int binarySearch() {
+
+  }
+  int equalSubstring(string s, string t, int maxCost) {
+    int n = s.size();
+    vector<int> accDiff(n + 1, 0);
+    
+  }
+};
+// @lc code=end
+
