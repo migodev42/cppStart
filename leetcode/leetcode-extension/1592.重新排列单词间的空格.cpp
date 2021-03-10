b09c8605db6ddeedf63db0705dce2af1901e3011
LeoooY <leoy.dev@gmail.com>
2021-03-10 19:18:31 +0800

    反转字符串2 替换空格

---
+++ leetcode/leetcode-extension/1592.重新排列单词间的空格.cpp
@@ -0,0 +1,15 @@
+/*
+ * @lc app=leetcode.cn id=1592 lang=cpp
+ *
+ * [1592] 重新排列单词间的空格
+ */
+
+// @lc code=start
+class Solution {
+public:
+    string reorderSpaces(string text) {
+
+    }
+};
+// @lc code=end
+
--- leetcode/leetcode-extension/541.反转字符串-ii.cpp
+++ leetcode/leetcode-extension/541.反转字符串-ii.cpp
@@ -3,13 +3,30 @@
  *
  * [541] 反转字符串 II
  */
+#include <algorithm>
+#include <string>
 
+using std::string;
+using std::swap;
 // @lc code=start
 class Solution {
-public:
+   public:
     string reverseStr(string s, int k) {
-
+        int i = 0;
+        while (i < s.size()) {
+            int left = i;
+            int right = i + k - 1;  // 左闭右闭
+            if (right >= s.size() - 1) {
+                right = s.size() - 1;
+            }
+            while (left < right) {
+                swap(s[left], s[right]);
+                ++left;
+                --right;
+            }
+            i += 2 * k;
+        }
+        return s;
     }
 };
 // @lc code=end
-
