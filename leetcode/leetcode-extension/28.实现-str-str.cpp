25d6860bbb65e8a3ce46e86e1d6965fa104e9e13
LeoooY <leoy.dev@gmail.com>
2021-03-16 14:14:51 +0800

    kmp first learn

--- leetcode/leetcode-extension/28.实现-str-str.cpp
+++ leetcode/leetcode-extension/28.实现-str-str.cpp
@@ -6,8 +6,8 @@
 #include <string>
 #include <vector>
 
-using std::vector;
 using std::string;
+using std::vector;
 // @lc code=start
 /* 暴力穷举法:超过时间限制 */
 // class Solution {
@@ -33,19 +33,48 @@ using std::string;
 //     }
 // };
 
-
 /* KMP算法 */
 class Solution {
    public:
-    void getNext(vector<int> &next, const string &s){
-
+    void getNext(vector<int> &next, const string &s) {
+        int j = -1;
+        next[0] = j;
+        for (int i = 1; i < next.size(); ++i) {
+            while (j >= 0 && s[i] != s[j + 1]) {  // 前后缀不同了
+                j = next[j];                      // 向前回溯
+            }
+            if (s[i] == s[j + 1]) {  // 找到相同后缀
+                j++;
+            }
+            next[i] = j;  // 将j（前缀的长度）赋值给next[i]
+        }
     }
     int strStr(string haystack, string needle) {
         if (!needle.size()) return 0;
-        int cursor = 0;
 
         vector<int> next(needle.size());
+        getNext(next, needle);
+
+        /* 比较 */
+        int j = -1;                                   // next数组起始为-1
+        for (auto i = 0; i < haystack.size(); ++i) {  // 注意i从0开始
+            while (j >= 0 && haystack[i] != needle[j + 1]) {  // 不匹配
+                j = next[j];  // 寻找之前匹配的位置
+            }
+            if (haystack[i] == needle[j + 1]) {  // 匹配，j和i同时向后移动
+                ++j;                             // i的增加在for循环里
+            }
+            if (j == (needle.size() - 1)) {  // 文本串出现了模式串
+                return i - needle.size() + 1;
+            }
+        }
+
         return -1;
     }
 };
 // @lc code=end
+
+int main(){
+  Solution s;
+  s.strStr("mississippi","issip");   // ""mississippi" \n"issip""
+}
