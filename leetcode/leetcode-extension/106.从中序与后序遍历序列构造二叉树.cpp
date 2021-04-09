a44173672e57662c4660ea20dd09ec54a9300bb0
LeoooY <leoy.dev@gmail.com>
2021-04-09 19:23:18 +0800

    中序后序构建二叉树

--- leetcode/leetcode-extension/106.从中序与后序遍历序列构造二叉树.cpp
+++ leetcode/leetcode-extension/106.从中序与后序遍历序列构造二叉树.cpp
@@ -32,7 +32,36 @@ struct TreeNode {
 class Solution {
    public:
     TreeNode *buildTree(vector<int> &inorder, vector<int> &postorder) {
-      
+        if (inorder.empty() || postorder.empty()) return nullptr;
+        if (postorder.empty()) return nullptr;
+        int rootidx = postorder.size() - 1;
+        int rootval = postorder[rootidx];
+
+        TreeNode *root = new TreeNode(rootval);
+
+        if (postorder.size() == 1) return root;
+
+        int split_idx = 0;
+        for (int i = 0; i < inorder.size(); ++i) {
+            if (rootval == inorder[i]) {
+                split_idx = i;
+                break;
+            }
+        }
+        vector<int> l_inorder(inorder.begin(), inorder.begin() + split_idx);
+        vector<int> r_inorder(inorder.begin() + split_idx + 1, inorder.end());
+
+        postorder.resize(postorder.size() - 1);
+
+        vector<int> l_postorder(postorder.begin(),
+                                postorder.begin() + l_inorder.size());
+        vector<int> r_postorder(postorder.begin() + l_inorder.size(),
+                                postorder.end());
+
+        root->left = buildTree(l_inorder, l_postorder);
+        root->right = buildTree(r_inorder, r_postorder);
+
+        return root;
     }
 };
 // @lc code=end
---
+++ leetcode/leetcode-extension/654.最大二叉树.cpp
@@ -0,0 +1,26 @@
+/*
+ * @lc app=leetcode.cn id=654 lang=cpp
+ *
+ * [654] 最大二叉树
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* constructMaximumBinaryTree(vector<int>& nums) {
+
+    }
+};
+// @lc code=end
+
