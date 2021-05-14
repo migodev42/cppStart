0a6f37942ebb9e2723948c4aeb8c440f39dce57c
LeoooY <leoy.dev@gmail.com>
2021-05-14 19:16:35 +0800

    chore(77): tODO

--- leetcode/leetcode-extension/77.组合.cpp
+++ leetcode/leetcode-extension/77.组合.cpp
@@ -9,10 +9,21 @@ using std::vector;
 
 // @lc code=start
 class Solution {
-public:
+   public:
+    vector<vector<int>> result;
+    void backtrack(int n, int k, vector<int> path) {
+        if (path.size() == k) {
+            result.push_back(path);
+            return;
+        }
+        for (int i = 0; i < n; ++i) {
+            path.push_back(i);
+            backtrack(n, k, )
+        }
+    }
     vector<vector<int>> combine(int n, int k) {
-
+        backtrack(n, k, {});
+        return result;
     }
 };
 // @lc code=end
-
