50be5649c1adfd4f67c7bc4df936bde417dac17b
LeoooY <leoy.dev@gmail.com>
2021-05-21 15:07:38 +0800

    feat(回溯): 回溯开始 简单的剪枝

--- leetcode/leetcode-extension/77.组合.cpp
+++ leetcode/leetcode-extension/77.组合.cpp
@@ -8,21 +8,50 @@
 using std::vector;
 
 // @lc code=start
-class Solution {
+/* 基本解法 */
+/* class Solution {
    public:
     vector<vector<int>> result;
-    void backtrack(int n, int k, vector<int> path) {
+    vector<int> path;
+    void backtrack(int n, int k, int curr) {
         if (path.size() == k) {
             result.push_back(path);
             return;
         }
-        for (int i = 0; i < n; ++i) {
-            path.push_back(i);
-            backtrack(n, k, )
+        for (int i = curr; i <= n; ++i) {
+            path.push_back(i); // 处理节点
+            backtrack(n, k, i + 1); //
+递归：控制树的纵向遍历，注意下一层搜索要从i+1开始 path.pop_back(); //
+回溯，撤销处理的节点
         }
     }
     vector<vector<int>> combine(int n, int k) {
-        backtrack(n, k, {});
+        backtrack(n, k, 1);
+        return result;
+    }
+}; */
+
+/* 剪枝 */
+class Solution {
+   public:
+    vector<vector<int>> result;
+    vector<int> path;
+    void backtrack(int n, int k, int curr) {
+        if (path.size() == k) {
+            result.push_back(path);
+            return;
+        }
+        for (int i = curr; i <= n - (k - path.size()) + 1; /* 剪枝 */
+             ++i) {
+            path.push_back(i);  // 处理节点
+            backtrack(
+                n, k,
+                i + 1);  // 递归：控制树的纵向遍历，注意下一层搜索要从i+1开始
+            path.pop_back();  // 回溯，撤销处理的节点
+        }
+    }
+    vector<vector<int>> combine(int n, int k) {
+        backtrack(n, k, 1);
         return result;
     }
 };
