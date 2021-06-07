830a34d64581685a41d52701ede41043e7d9f73f
LeoooY <leoy.dev@gmail.com>
2021-06-07 12:14:39 +0800

    feat(回溯): 17.电话号码的字母组合 常规回溯

--- leetcode/leetcode-extension/17.电话号码的字母组合.cpp
+++ leetcode/leetcode-extension/17.电话号码的字母组合.cpp
@@ -14,49 +14,41 @@ using std::vector;
 // @lc code=start
 class Solution {
    private:
-    unordered_map<char, vector<char>> numberMaps = {
-        // {'a', 2}, {'b', 2}, {'c', 2}, {'d', 3}, {'e', 3}, {'f', 3},
-        // {'g', 4}, {'h', 4}, {'i', 4}, {'j', 5}, {'k', 5}, {'l', 5},
-        // {'m', 6}, {'n', 6}, {'o', 6}, {'p', 7}, {'q', 7}, {'rs', 7},
-        // {'t', 8}, {'u', 8}, {'v', 8}, {'w', 9}, {'x', 9}, {'yz', 9},
-        {'2', {'a', 'b', 'c'}}, {'3', {'d', 'e', 'f'}},
-        {'4', {'g', 'h', 'i'}}, {'5', {'j', 'k', 'l'}},
-        {'6', {'m', 'n', 'o'}}, {'7', {'p', 'q', 'r', 's'}},
-        {'8', {'t', 'u', 'v'}}, {'9', {'w', 'x', 'y', 'z'}},
+    vector<string> letterMap = {
+        "",
+        ""
+        "abc",
+        "def",
+        "ghi",
+        "jkl",
+        "mno",
+        "pqrs",
+        "tuv",
+        "wxyz",
     };
-    vector<vector<char>> targetMap;
     vector<string> result;
-    int size = 0;
+    string path;
 
    public:
-    void backtrack(string curr) {
-        if (curr.size() == size) {
-            result.push_back(curr);
+    void backtrack(const string& digits, int index) {
+        if (path.size() == digits.size()) {
+            result.push_back(path);
             return;
         }
 
-        for (int i = curr.size(); i <= size; ++i) {
-            backtrack(curr + targetMap[i][i]);
+        int digit = digits[index] - '0';        // string 转 int
+        string letters = letterMap[digit - 1];  // 取当前数字对应字符集
+        for (int i = 0; i < letters.size(); ++i) {
+            path.push_back(letters[i]);  // 处理
+            backtrack(
+                digits,
+                index + 1);  // 递归，注意index+1，一下层要处理下一个数字了
+            path.pop_back();  // 回溯
         }
     }
     vector<string> letterCombinations(string digits) {
-        size = digits.size();
-
-        for (char digit : digits) {
-            targetMap.push_back(numberMaps[digit]);
-        }
-        backtrack();
-
-        /* 打印selectTarget */
-        // int cnt = 0;
-        // for(auto item : targetMap){
-        //   cout << digits[cnt++] << " : 当前数字";
-        //   for(auto i : item){
-        //     cout <<  i << " ";
-        //   }
-        //   cout << endl;
-        // }
-
+        if (!digits.size()) return result;
+        backtrack(digits, 0);
         return result;
     }
 };
