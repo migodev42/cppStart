d74f506bcfad3164ab22c86bb09bd3749dd4dfb8
LeoooY <leoy.dev@gmail.com>
2021-07-05 12:06:11 +0800

    feat(贪心): 122.买卖股票的最佳时机 II

--- leetcode/leetcode-extension/122.买卖股票的最佳时机-ii.cpp
+++ leetcode/leetcode-extension/122.买卖股票的最佳时机-ii.cpp
@@ -4,12 +4,22 @@
  * [122] 买卖股票的最佳时机 II
  */
 
+#include <limits.h>
+#include <algorithm>
+#include <vector>
+
+using std::vector;
+using std::max;
+
 // @lc code=start
 class Solution {
-public:
+   public:
     int maxProfit(vector<int>& prices) {
-
+        int result = 0;
+        for (int i = 1; i < prices.size(); ++i) {
+            result += max(prices[i]-prices[i-1],0);
+        }
+        return result;
     }
 };
 // @lc code=end
-
