01a2fb1f1794d3c3595a081f17f27bc996717659
leoooy <leoy.dev@gmail.com>
2021-11-13 23:46:14 +0800

    TODO: 整数拆分

--- leetcode/leetcode-extension/343.整数拆分.cpp
+++ leetcode/leetcode-extension/343.整数拆分.cpp
@@ -4,14 +4,18 @@
  * [343] 整数拆分
  */
 
+#include <vector>
+#include <algorithm>
+
+using std::max;
+using std::vector;
 // @lc code=start
 
-
 // 动态规划
-// dp[n] = 
+// dp[n] =
 
-/* 
-      2   3   4   5 
+/*
+      2   3   4   5
   1   1   1   1   1
   2   -   2   4   4
   3
@@ -24,10 +28,17 @@
 
  */
 class Solution {
-public:
-    int integerBreak(int n) {
-
+ public:
+  int integerBreak(int n) {
+    vector<int> dp(n+1);
+    // n >= 3
+    dp[2] = 1;
+    for (int i = 3; i <= n; i++) {
+      for (int j = 1; j < i - 1; j++) {
+        dp[i] = max(dp[i], max((i - j) * j, dp[i - j] * j));
+      }
     }
+    return dp[n];
+  }
 };
 // @lc code=end
-
